6f649d2c948002919cf49f9096f725a4d39d6e1e
Paulius Požarskis <paulius@bluemint.dev>
2025-12-03 14:58:07 +0200

    VIPPS-25 allow discount code before Vipps user is loaded

--- Controller/Vipps/UpdateTotals.php
+++ Controller/Vipps/UpdateTotals.php
@@ -165,7 +165,7 @@ class UpdateTotals implements ActionInterface, CsrfAwareActionInterface
         if ($forceUpdate || $currentTotal !== $vippsTotal) {
             if ($data['shippingId']) {
                 $this->updateVippsSession($quote, $data['shippingId']);
-            } else {
+            } elseif ($quote->getShippingAddress()->getCustomerEmail()) {
                 $this->updateVippsSession($quote);
             }
         }
